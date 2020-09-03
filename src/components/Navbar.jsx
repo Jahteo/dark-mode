eb2e388f40255d01cbb7b8000b1f919a6774c7be
Josh Glantz-Hucks <jahteo@gmail.com>
2020-09-02 22:03:30 -0700

    Idk how this allows the toggle to still work. but it doesn't actually set anything to localStorage, which was the whole goal of the exercise. Not a clue why not. Not a clue why it should.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
+import useDarkMode from "../hooks/useDarkMode"
 
 const Navbar = ({darkMode, setDarkMode}) => {
   // const [darkMode, setDarkMode] = useState(false);
-  const toggleMode = e => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+  // const toggleMode = e => {
+  //   e.preventDefault();
+  //   setDarkMode(!darkMode);
+  // };
+  useDarkMode(darkMode, setDarkMode)
+
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
       <div className="dark-mode__toggle">
         <div
-          onClick={toggleMode}
+          // onClick={toggleMode}
+          onClick={() => setDarkMode}
+          // onClick={() => setDarkMode()}
           className={darkMode ? 'toggle toggled' : 'toggle'}
         />
       </div>
--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,4 +1,4 @@
-import { useLocalStorage } from "./useLocalStorage";
+import useLocalStorage from "./useLocalStorage";
 
 const useDarkMode = (key) => {
   const [darkModeEnabled, setDarkModeEnabled] = useLocalStorage(key);
